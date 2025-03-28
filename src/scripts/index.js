6fdf73be8605a6b196caee5c28ae28acd836cc6f
riyu.iring <oribelloruthiemy@gmail.com>
2025-03-29 00:07:45 +0800

    height fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -36,8 +36,8 @@ const displayFilm = (data)=>{
     const {title, original_title, image, movie_banner, description, director, id}  = data;
     return displayFilmContainer.innerHTML += 
     `
-        <a id="${id}" href="/src/pages/film.html?id=${id}" class="obsAppear opacity-0 flex gap-2 bg-zinc-900 rounded-md max-w-[500px] h-[250px] text-white overflow-hidden shadow-md transition-opacity duration-700">
-            <img src="${image}" class="max-h-full">
+        <a id="${id}" href="/src/pages/film.html?id=${id}" class="obsAppear opacity-0 flex gap-2 bg-zinc-900 rounded-md max-w-[500px] h-fit text-white overflow-hidden shadow-md transition-opacity duration-700">
+            <img src="${image}" class="max-h-[250px] h-[250px] ">
             <div class="py-3 pl-2 pr-3 flex-col flex gap-2 justify-between">
                 <div class="flex-col flex gap-2">
                     <p class="text-md text-zinc-100 font-medium">${title}</p>
